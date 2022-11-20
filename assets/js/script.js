c226c28798836f9acf6d48357771b118568d36df
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2022-11-19 19:32:15 -0700

    add main files to Repo

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// Assignment Code
+
+
+
+function generatePassword(){
+var myNewPassword = "    ",
+
+
+
+confirm()
+alert()
+prompt()
+
+parseInt()
+
+Math.floor(math.random() * Array.length)
+
+
+
+  return
+}
+
+
+
+
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
