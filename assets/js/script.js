0913a94f1d58e47e1d0b2be5ca708dec2a3f0eea
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2022-11-27 20:54:18 -0700

    finished(?)

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,19 +36,20 @@ var userNumbers;
 spChars = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "[", "]", "|", ":", ";","'", '"', "<", ">", ",", ".", "?"];
 var userSpChars;
 
-
+var pWordLength = "";
 
 function generatePassword() {
 
   // Password length criteria
+var p
 var pWordLength = prompt("How long do you want your password to be?");
 
-// Tried if else, but while works better
+// Tried if else statement, but while statement works better
 while (pWordLength < 8 || pWordLength > 128) {
   alert("Your password must be between 8 and 128 characters. Please enter new password length.");
   var pWordLength = prompt("How long do you want your password to be?");
 }
-alert("Lets see what type of characters you want to incorporate in your new password. 'Ok' for yes and 'cancel' for no.")
+alert("YAY! 😀 Lets see what type of characters you want to incorporate in your new password. 'Ok' for yes and 'cancel' for no.")
 
 
 // Characters for password prompts
@@ -57,7 +58,16 @@ var userUpperLetters = confirm("Would you like upper case letters in you passwor
 var userNumbers = confirm("Would you like numbers in your password?")
 var userSpChars = confirm("would you like special characters in your password?")
 
+// If the user doesn't select any character types
+while (userLowerLetters !== true && userUpperLetters !== true && userNumbers !== true && userSpChars !== true) {
+  alert("Ooops 😥, looks like you did not select any character type(s). At least one character type must be selected to create password.")
+  var userLowerLetters = confirm("Would you like lower case letters in your password?");
+  var userUpperLetters = confirm("Would you like upper case letters in you password?");
+  var userNumbers = confirm("Would you like numbers in your password?")
+  var userSpChars = confirm("would you like special characters in your password?")
+}
 
+// Making the character types able to be combined
 var charSoup = [];
 if (userLowerLetters) {
     charSoup = charSoup.concat(lowerLetters);
@@ -74,7 +84,7 @@ if (userSpChars) {
 
 // console.log(charSoup);
 
-
+// Create new password
 var newPassword = "";
 for (var i = 0; i < pWordLength; i++) {
   newPassword = newPassword + charSoup[Math.floor(Math.random() * charSoup.length)];
